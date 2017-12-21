abcb68c4b9501562c9c6c3c997bf311c13f20373
kaixin138 <784665813@qq.com>
2017-12-21 15:02:10 +0800

    图像分析打标

---
+++ 图像分析打标/AESDecode.java
@@ -0,0 +1,152 @@
+package org.spring.springboot.dubbo;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+@SuppressWarnings("restriction")
+public class AESDecode {
+    /*
+    * 计算MD5+BASE64
+    */
+    public static String MD5Base64(String s) {
+        if (s == null)
+            return null;
+        String encodeStr = "";
+        byte[] utfBytes = s.getBytes();
+        MessageDigest mdTemp;
+        try {
+            mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(utfBytes);
+            byte[] md5Bytes = mdTemp.digest();
+            BASE64Encoder b64Encoder = new BASE64Encoder();
+            encodeStr = b64Encoder.encode(md5Bytes);
+        } catch (Exception e) {
+            throw new Error("Failed to generate MD5 : " + e.getMessage());
+        }
+        return encodeStr;
+    }
+
+    /*
+   * 计算 HMAC-SHA1
+   */
+    public static String HMACSha1(String data, String key) {
+        String result;
+        try {
+            SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), "HmacSHA1");
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(signingKey);
+            byte[] rawHmac = mac.doFinal(data.getBytes());
+            result = (new BASE64Encoder()).encode(rawHmac);
+        } catch (Exception e) {
+            throw new Error("Failed to generate HMAC : " + e.getMessage());
+        }
+        return result;
+    }
+
+    /*
+   * 等同于javaScript中的 new Date().toUTCString();
+   */
+    public static String toGMTString(Date date) {
+        SimpleDateFormat df = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.UK);
+        df.setTimeZone(new java.util.SimpleTimeZone(0, "GMT"));
+        return df.format(date);
+    }
+
+    /*
+   * 发送POST请求
+   */
+    public static String sendPost(String url, String body, String ak_id, String ak_secret) throws Exception {
+        PrintWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        int statusCode = 200;
+        try {
+            URL realUrl = new URL(url);
+            /*
+             * http header 参数
+             */
+            String method = "POST";
+            String accept = "application/json";
+            String content_type = "application/json";
+            String path = realUrl.getFile();
+            String date = toGMTString(new Date());
+            // 1.对body做MD5+BASE64加密
+            String bodyMd5 = MD5Base64(body);
+            String stringToSign = method + "\n" + accept + "\n" + bodyMd5 + "\n" + content_type + "\n" + date + "\n"
+                    + path;
+            // 2.计算 HMAC-SHA1
+            String signature = HMACSha1(stringToSign, ak_secret);
+            // 3.得到 authorization header
+            String authHeader = "Dataplus " + ak_id + ":" + signature;
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", accept);
+            conn.setRequestProperty("content-type", content_type);
+            conn.setRequestProperty("date", date);
+            conn.setRequestProperty("Authorization", authHeader);
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            // 发送请求参数
+            out.print(body);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            statusCode = ((HttpURLConnection) conn).getResponseCode();
+            if (statusCode != 200) {
+                in = new BufferedReader(new InputStreamReader(((HttpURLConnection) conn).getErrorStream()));
+            } else {
+                in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            }
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (statusCode != 200) {
+            throw new IOException("\nHttp StatusCode: " + statusCode + "\nErrorMessage: " + result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // 发送POST请求示例
+        String ak_id = "LTAIjPRMl8bQdj7U"; //用户ak
+        String ak_secret = "Ld7WoG4d0ICWjfFdpNrLS8q7E3VqXR"; // 用户ak_secret
+        String url = "https://dtplus-cn-shanghai.data.aliyuncs.com/image/tag";
+        String body = "{\"type\":0,\"image_url\":\"http://wanglittlehandsome.oss-cn-beijing.aliyuncs.com/wangyao/image/liuyifei.jpg\"}";
+        String aaa = sendPost(url, body, ak_id, ak_secret);
+        System.out.println("response body:" + aaa);
+        JSONArray array = JSONArray.parseArray(JSON.parseObject(aaa).get("tags").toString());
+        Object[] labels = array.toArray();
+        System.out.println(labels);
+    }
+}
